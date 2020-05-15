b517ed886cc728c357aa7616f5ccc23d8146ec34
super <supeng842499467@gmail.com>
2020-05-15 23:05:17 +0800

    nn

--- MultiVariableLinearRegression/NeuralNetwork-keras.py
+++ MultiVariableLinearRegression/NeuralNetwork-keras.py
@@ -10,18 +10,21 @@ from keras.layers import Dense
 from keras.models import Sequential
 from keras.callbacks import EarlyStopping
 
-from 
+from sklearn.preprocessing import StandardScaler
 
 from HelperClass.DataReader_1_0 import *
 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def get_data():
     sdr = DataReader_1_0("../data/ch05.npz")
     sdr.ReadData()
     X,Y = sdr.GetWholeTrainSamples()
-
+    ss = StandardScaler()
+    X = ss.fit_transform(X)
+    Y = ss.fit_transform(Y)
     return X, Y
 
 
@@ -33,16 +36,30 @@ def build_model():
     return model
 
 
-def plt_data(x, y, model):
-    # draw sample data
-    plt.plot(x, y, "b.")
-    # draw predication data
-    PX = np.linspace(0,1,10)
-    PZ = model.predict(PX)
-    plt.plot(PX, PZ, "r")
-    plt.title("Air Conditioner Power")
-    plt.xlabel("Number of Servers(K)")
-    plt.ylabel("Power of Air Conditioner(KW)")
+def plt_3d(model, X, Y):
+    # draw example points
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(X[:,0],X[:,1],Y)
+    p = np.linspace(-2,1)
+    q = np.linspace(-2,2)
+    P,Q = np.meshgrid(p,q)
+    R = np.hstack((P.ravel().reshape(2500,1), Q.ravel().reshape(2500,1)))
+    Z = model.predict(R)
+    Z = Z.reshape(50,50)
+    ax.plot_surface(P,Q,Z, cmap='rainbow')
+    plt.show()
+
+
+def plt_loss(history):
+    loss = history.history['loss']
+    epochs = range(1, len(loss) + 1)
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.title('Training  loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
     plt.show()
 
 
@@ -52,11 +69,13 @@ if __name__ == '__main__':
     y = np.array(Y)
     print(x.shape)
     print(y.shape)
+    print(x)
 
     model = build_model()
     # patience设置当发现loss没有下降的情况下，经过patience个epoch后停止训练
     early_stopping = EarlyStopping(monitor='loss', patience=100)
-    model.fit(x, y, epochs=100, batch_size=10, callbacks=[early_stopping])
+    history = model.fit(x, y, epochs=100, batch_size=32, callbacks=[early_stopping])
     w, b = model.layers[0].get_weights()
     print(w, b)
-    # plt_data(x, y, model)
+    plt_3d(model, x, y)
+    plt_loss(history)
